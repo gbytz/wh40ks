7f585734c09b3b304ef69b8fd8ed0fbab509e5cc
gbytz <gbytz.zyt@gmail.com>
2023-02-25 08:08:57 +0200

    Add entities creation

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+#define ENTITIES_LIMIT 64
+
+uint64_t entities = 0;
+uint64_t create_entity()
+{
+    return ++entities;
+}
+
 int main(int argc, char const *argv[])
 {
+    printf("entities: %lu\n", entities);
+    uint64_t id = create_entity();
+    printf("entities: %lu\n", entities);
+    printf("entity: %lu\n", id);
+
     return 0;
 }
